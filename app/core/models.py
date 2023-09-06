5395467d82965b4dbb55cf07ac7e4659303a5869
it-AVNG <it.avng@gmail.com>
2023-09-06 14:35:01 +0700

    add test for custom user creation

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models # noqa
+'''
+database models
+'''
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    '''manager for user'''
+    def create_user(self,email, password=None, **extra_fields):
+        '''create,save,return a newuser'''
+        user=self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''user in the system.'''
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD ='email'
+
